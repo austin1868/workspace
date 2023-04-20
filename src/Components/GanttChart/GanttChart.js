a8004cff2be1199ff75bd37f38685e595ed56e5b
Austin Witt <106761550+austin1868@users.noreply.github.com>
2023-04-20 01:46:27 +0000

    Update GanttChart.js

--- src/Components/GanttChart/GanttChart.js
+++ src/Components/GanttChart/GanttChart.js
@@ -1,35 +1,43 @@
-import React, { useEffect } from "react";
-import { GanttChart as ToastGanttChart } from "@toast-ui/react-gantt-chart";
-import "tui-gantt/dist/tui-gantt.min.css";
+import React, { useEffect, useRef } from "react";
+import TuiGanttChart from "tui-gantt";
+import "tui-gantt/dist/tui-gantt.css";
 import "./GanttChart.css";
 
 function GanttChart() {
-  const data = {
-    columns: [
-      { name: "Task", id: "title" },
-      { name: "Start Date", id: "start" },
-      { name: "End Date", id: "end" }
-    ],
-    tasks: [
-      {
-        id: 1,
-        title: "Task 1",
-        start: "2023-05-01",
-        end: "2023-05-05"
-      },
-      {
-        id: 2,
-        title: "Task 2",
-        start: "2023-05-06",
-        end: "2023-05-09"
-      }
-    ]
-  };
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const data = {
+        columns: [
+          { name: "Task", id: "title" },
+          { name: "Start Date", id: "start" },
+          { name: "End Date", id: "end" },
+        ],
+        tasks: [
+          {
+            id: 1,
+            title: "Task 1",
+            start: "2023-05-01",
+            end: "2023-05-05",
+          },
+          {
+            id: 2,
+            title: "Task 2",
+            start: "2023-05-06",
+            end: "2023-05-09",
+          },
+        ],
+      };
+
+      new TuiGanttChart(chartRef.current, data, {});
+    }
+  }, []);
 
   return (
     <div className="gantt-chart">
       <h1>Gantt Chart</h1>
-      <ToastGanttChart data={data} />
+      <div ref={chartRef}></div>
     </div>
   );
 }
